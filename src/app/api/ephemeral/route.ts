e3a2888e6b706da330efa28dd92d5b61646bb9e4
3obby <rfusseryiii+gh@gmail.com>
2026-03-03 21:25:19 -0800

    fix

--- src/app/api/ephemeral/route.ts
+++ src/app/api/ephemeral/route.ts
@@ -44,10 +44,7 @@ export async function GET(request: Request) {
     };
     if (parentHash) where.parentContentHash = parentHash;
     if (topicHash) where.topicHash = topicHash;
-    if (root) {
-      where.parentContentHash = null;
-      where.parentNostrId = null;
-    }
+    if (root) where.parentContentHash = null;
     if (pubkey) where.nostrPubkey = pubkey;
 
     const orderBy =
